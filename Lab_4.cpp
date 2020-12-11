3007ea838f94b6d80961850f68867df4fd0c4b73
nxtSSanya <56893467+nxtSSanya@users.noreply.github.com>
2020-12-11 14:07:23 +0400

    Update Lab_4.cpp

--- Lab_4.cpp
+++ Lab_4.cpp
@@ -32,11 +32,11 @@ const int INF = 1 << 30;
 const ll LINF = 1ll << 62;
 
 class Lab_4 {
-	vector< vector <int> > m;
+	vector< vector <double> > m;
 
 public:
 	void set_matrix() {
-		vector<int> a1 = { 5,5,7 }, a2 = { 3, 4, 6 }, a3 = { 2, 4 ,8 };
+		vector<double> a1 = { 3, 3.3, 5.04 }, a2 = { 1.35, 1.155, 1.26 }, a3 = { 0.09, 0.0975 ,0.12 };
 		m.reserve(3);
 		this->m.push_back(a1);
 		this->m.push_back(a2);
@@ -45,23 +45,32 @@ public:
 	void get_matrix() {
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
-				cout << this->m[i][j] << " ";
+				cout << this->m[i][j] << "\t";
 			}
 			cout << endl;
 		}
 	}
 	void Kriteriy_Valda() {
-		vector<int> mx(3, INF);
-			for (int j = 0; j < 3; ++j) {
-				mx[0] = min(this->m[0][j], mx[0]);
-				mx[1] = min(this->m[1][j], mx[1]);
-				mx[2] = min(this->m[2][j], mx[2]);
-			}
-			cout << "maximum of income: " << *max_element(mx.begin(), mx.end()) << endl;
+		cout << "----------------------" << endl;
+		cout << "KRITERIY VALDA" << endl;
+		vector<double> mx(3, INF);
+		vector< pair <int, int > > ind(3);
+		for (int j = 0; j < 3; ++j) {
+			mx[0] = min(this->m[0][j], mx[0]);
+			mx[1] = min(this->m[1][j], mx[1]);
+			mx[2] = min(this->m[2][j], mx[2]);
+		}
+		ind = this->GetIndex(m, mx);
+		cout << "For A1: " << mx[0] << endl;
+		cout << "For A2: " << mx[1] << endl;
+		cout << "For A3: " << mx[2] << endl;
+		cout << "maximum of {" << mx[0] << ", " << mx[1] << ", " << mx[2] << "}: " << *max_element(mx.begin(), mx.end()) << endl;
+		cout << "----------------------" << endl;
 	}
 	void Kriteriy_Gurvitsa() {
+		cout << "KRITERIY GURVITSA" << endl;
 		const double A = 0.3;
-		vector<int> mx(3, -INF), mn(3, INF);
+		vector<double> mx(3, -INF), mn(3, INF);
 		vector<double> res(3);
 		for (int i = 0; i < 3; ++i) {
 			mn[0] = min(this->m[0][i], mn[0]);
@@ -74,7 +83,54 @@ public:
 		for (int q = 0; q < 3; ++q) {
 			res[q] = A * mx[q] + (1 - A)*mn[q];
 		}
-		cout << "maximum of income: " << *max_element(res.begin(), res.end()) << endl;
+		cout << "For A1: " << A << "*" << mx[0] << "+" << (1 - A) << "*" << mn[0] << " = " << res[0] << endl;
+		cout << "For A2: " << A << "*" << mx[1] << "+" << (1 - A) << "*" << mn[1] << " = " << res[1] << endl;
+		cout << "For A3: " << A << "*" << mx[2] << "+" << (1 - A) << "*" << mn[2] << " = " << res[2] << endl;
+		cout << "maximum of {" << res[0] << ", " << res[1] << ", " << res[2] << "}: " << *max_element(res.begin(), res.end()) << endl;
+		cout << "----------------------" << endl;
+	}
+	void Kriteriy_Sevidzha() {
+		cout << "KRITERIY SEVIDZHA" << endl;
+		vector<double> mx(3, -INF);
+		vector<int> ind(3);
+		double res(3);
+		for (int i = 0; i < 3; ++i) {
+			mx[0] = max(this->m[0][i], mx[0]);
+			mx[1] = max(this->m[1][i], mx[1]);
+			mx[2] = max(this->m[2][i], mx[2]);
+		}
+		cout << "For A1: " << mx[0] << endl;
+		cout << "For A2: " << mx[1] << endl;
+		cout << "For A3: " << mx[2] << endl;
+		cout << "minimum of {" << mx[0] << ", " << mx[1] << ", " << mx[2] << "}: " << *min_element(mx.begin(), mx.end()) << endl;
+		cout << "----------------------" << endl;
+	}
+	void Kriteriy_max() {
+		cout << "KRITERIY MAXIMUMA" << endl;
+		vector<double> mx(3, -INF);
+		for (int i = 0; i < 3; ++i) {
+			mx[0] = max(this->m[0][i], mx[0]);
+			mx[1] = max(this->m[1][i], mx[1]);
+			mx[2] = max(this->m[2][i], mx[2]);
+		}
+		cout << "For A1: " << mx[0] << endl;
+		cout << "For A2: " << mx[1] << endl;
+		cout << "For A3: " << mx[2] << endl;
+		cout << "maximum of {" << mx[0] << ", " << mx[1] << ", " << mx[2] << "}: " << *max_element(mx.begin(), mx.end()) << endl;
+		cout << "----------------------" << endl;
+	}
+private:
+	vector<pair <int, int> > GetIndex(vector< vector<double> > a, vector<double> track) {
+		vector<pair<int, int> > ind(3);
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				if (track[i] == a[i][j]) {
+					ind[i].first = i;
+					ind[j].second = j;
+				}
+			}
+		}
+		return ind;
 	}
 };
 int main()
@@ -87,5 +143,7 @@ int main()
 	example.get_matrix();
 	example.Kriteriy_Valda();
 	example.Kriteriy_Gurvitsa();
+	example.Kriteriy_Sevidzha();
+	example.Kriteriy_max();
 	return 0;
 }
