fbd2047ff06870c936dd8757b9ce515a62350d1c
nxtSSanya <56893467+nxtSSanya@users.noreply.github.com>
2020-12-20 20:59:13 +0400

    Update Lab_4.cpp

--- Lab_4.cpp
+++ Lab_4.cpp
@@ -119,6 +119,19 @@ public:
 		cout << "maximum of {" << mx[0] << ", " << mx[1] << ", " << mx[2] << "}: " << *max_element(mx.begin(), mx.end()) << endl;
 		cout << "----------------------" << endl;
 	}
+	void Kriteriy_Laplasa() {
+		double q = 0.33;
+		vector<double> res(3, 0);
+		for (int i = 0; i < 3; ++i) {
+			res[0] = this->m[0][i] * q;
+			res[1] = this->m[1][i] * q;
+			res[2] = this->m[2][i] * q;
+		}
+		cout << "For A1: " << m[0][0] << " * " << q << " + " << m[0][1] << " * " << q << " + " << m[0][2] << " * " << q << " = " << res[0] << endl;
+		cout << "For A2: " << m[1][0] << " * " << q << " + " << m[1][1] << " * " << q << " + " << m[1][2] << " * " << q << " = " << res[1] << endl;
+		cout << "For A3: " << m[2][0] << " * " << q << " + " << m[2][1] << " * " << q << " + " << m[2][2] << " * " << q << " = " << res[2] << endl;
+		cout << "maximum of {" << res[0] << ", " << res[1] << ", " << res[2] << "}: " << *max_element(res.begin(), res.end()) << endl;
+	}
 private:
 	vector<pair <int, int> > GetIndex(vector< vector<double> > a, vector<double> track) {
 		vector<pair<int, int> > ind(3);
@@ -145,5 +158,6 @@ int main()
 	example.Kriteriy_Gurvitsa();
 	example.Kriteriy_Sevidzha();
 	example.Kriteriy_max();
+	example.Kriteriy_Laplasa();
 	return 0;
 }
